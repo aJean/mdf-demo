cf1590d735ec018180403959da8184626c0f52e3
qy <30001677@qq.com>
2021-04-14 20:31:59 +0800

    update

--- packages/react/src/global.d.ts
+++ packages/react/src/global.d.ts
@@ -37,3 +37,8 @@ declare module '*.sass' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
