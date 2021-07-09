7d4979f216efc64aa5a4ed03d8b8294d6942e3d2
qy <30001677@qq.com>
2021-07-09 16:07:23 +0800

    fix: update

--- packages/node/src/server/app.module.ts
+++ packages/node/src/server/app.module.ts
@@ -5,4 +5,16 @@ import OneModule from './one/one.module';
  * @file 启动模块
  */
 
-export default Helper.createAppModule({ imports: [OneModule] });
+export default Helper.createAppModule({
+  imports: [OneModule],
+  /**
+   * 自定义 http 异常处理
+   */
+  handleHttpError(err, req, res) {
+    console.log(err);
+    res.send({
+      code: 200,
+      msg: 'hahah',
+    });
+  },
+});
